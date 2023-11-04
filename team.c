c65bce7135034843dde76f05a3f1909b47932a7f
Parth Gujarkar <parthgujarkar1203@gmail.com>
2023-11-04 17:47:21 +0530

    Working code

--- team.c
+++ team.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
-int i;
+#include<string.h>
 struct player
 {
 	char name[50];
@@ -9,19 +9,28 @@ struct player
 
 void sort(struct player *p1, int n)
 {
-	for(int j=0;j<n-1;j++)
+	for(int i=0;i<n-1;i++)
 	{
-		float min=p1[j].bat_avg;
-		for(int i=j;i<n;i++)
+		for(int j=i;j<n;j++)
 		{
-			if(min>p1[i].bat_avg)
+			if(p1[j].bat_avg<p1[j+1].bat_avg)
 			{
-				float temp=p1[i].bat_avg;
-				p1[i].bat_avg=min;
-				min=temp;
+				float temp=p1[j].bat_avg;
+				p1[j].bat_avg=p1[j+1].bat_avg;
+				p1[j+1].bat_avg=temp;
+				
+				char temp_c[50];
+				char temp_n[50];
+				strcpy(temp_c,p1[j].country);
+				strcpy(temp_n,p1[j].name);
+
+				strcpy(p1[j].country,p1[j+1].country);
+				strcpy(p1[j].name,p1[j+1].name);
+
+				strcpy(p1[j+1].country,temp_c);
+				strcpy(p1[j+1].name,temp_n);
 			}
 		}
-		p1[j].bat_avg=min;
 	}
 }
 
@@ -32,7 +41,7 @@ int main()
 	scanf("%d", &n);
 	getchar();
 	struct player p[50];
-	for(i=0;i<n;i++)
+	for(int i=0;i<n;i++)
 	{
 		printf("\nEnter name:");
 		scanf("%[^\n]s",p[i].name);
@@ -46,9 +55,9 @@ int main()
 	}
 	sort(p,n);
 	printf("\n\nSorted List of Players:\n");
-	for(i=0;i<n;i++)
+	for(int i=0;i<n;i++)
 	{
-		printf("Name: %s \nCountry: %s\nBatting Average: %f\n\n",p[i].name,p[i].country,p[i].bat_avg);
+		printf("Name: %s \nCountry: %s\nBatting Average: %f\n\n",p[i].name,p[i].country,p[i].bat_avg);\
 	}
 	return 0;
 }
